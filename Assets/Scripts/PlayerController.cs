39889fafddc5bb6b7c3850750d1164429e36e8f5
Halthomas65 <thaihao.dp@gmail.com>
2024-08-06 15:12:59 +0700

    move1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,85 +4,77 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController controller;
     private Vector3 direction;
-    public float forwardSpeed = 10f;
+    public float forwardSpeed = 5.0f;
 
-    private int desiredLane = 1; // 0 = Left, 1 = Middle, 2 = Right
-    public float laneDistance = 4; // Distance between two lanes
+    public float jumpForce = 7.0f;
+    private Rigidbody rb;
+    public LayerMask groundLayer;
+    public float raycastDistance = 0.6f;
 
-    public float jumpForce = 10;
-    public float gravity = -20;
+    private int desiredlane = 1; // 0: Left, 1: Middle, 2: Right
+    public float laneDistance = 4.0f;
+
+    private bool isGrounded;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        direction.z = forwardSpeed;
+        // Check if the player is grounded
+        RaycastHit hit;
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance, groundLayer);
 
-        if (controller.isGrounded)
+        // Jump
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
         {
-            direction.y = -1;
-
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                Jump();
-            }
-        }
-        else
-        {
-            direction.y += gravity * Time.deltaTime;
-        }
-
-        // Gather the inputs on which lane we should be
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            desiredLane++;
-            if (desiredLane == 3)
-            {
-                desiredLane = 2;
-            }
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            desiredLane--;
-            if (desiredLane == -1)
+            desiredlane--;
+            if (desiredlane == -1)
             {
-                desiredLane = 0;
+                desiredlane = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredlane++;
+            if (desiredlane == 3)
+            {
+                desiredlane = 2;
             }
         }
 
         // Calculate where we should be in the future
+
         Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
 
-        if (desiredLane == 0)
+        if (desiredlane == 0)
         {
             targetPosition += Vector3.left * laneDistance;
         }
-        else if (desiredLane == 2)
+        else if (desiredlane == 2)
         {
             targetPosition += Vector3.right * laneDistance;
         }
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, 80 * Time.fixedDeltaTime);
-        // transform.position = targetPosition;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 80 * Time.deltaTime);
+
+        // Move the player forward
+        transform.position += new Vector3(0, 0, forwardSpeed) * Time.deltaTime;
     }
 
-    private void FixedUpdate()
-    {
-        // Debug.Log("Time: " + Time.fixedDeltaTime);
-        controller.Move(direction * Time.fixedDeltaTime);
-    }
 
     private void Jump()
     {
-        direction.y = jumpForce;
     }
 }
