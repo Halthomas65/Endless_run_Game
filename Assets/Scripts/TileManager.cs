476a105b02a25bb151088dea0ceb6a589c138769
Halthomas65 <thaihao.dp@gmail.com>
2024-08-10 23:43:52 +0700

    spawn tiles

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    public float zSpawn = 0;
+    public float tileLength = 30;
+    public int numberOfTiles = 3;
+    private List<GameObject> activeTiles;
+
+    public Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeTiles = new List<GameObject>();
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            if (i == 0)
+            {
+                SpawnTile(0);
+            }
+            else
+            {
+                SpawnTile(Random.Range(1, tilePrefabs.Length));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform.position.z - 35 > zSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(Random.Range(1, tilePrefabs.Length));
+            DeleteTile();
+        }
+    }
+
+    public void SpawnTile(int tileIndex = 0)
+    {
+        GameObject go = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        activeTiles.Add(go);
+
+        zSpawn += tileLength;
+    }
+
+    private void DeleteTile()
+    {
+        // Destroy the first tile in the list
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
